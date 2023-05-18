ef6d50e139ce5c10b1ee643fea3753f1110481d8
jason <jason36375667@gmail.com>
2023-05-18 17:53:27 +0800

    good

--- html2023-spring-final-project/main.py
+++ html2023-spring-final-project/main.py
@@ -1,16 +1,40 @@
 import numpy as np
-import csv
-from liblinear.liblinearutil import *
+import pandas as pd
 
-with open('train.csv', newline='', encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    data_list_t = list(reader)
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error
 
-data_list = np.array(data_list_t).T.tolist()
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+sample_df = pd.read_csv('submission.csv')
 
-# print(len(data_list[17]))
-# print(data_list[17][len(data_list[17])-1])
+y = train_df[['Danceability']].to_numpy().squeeze()
+use_list = ['Energy', 'Key', 'Loudness', 'Speechiness', 'Acousticness', 'Instrumentalness',
+            'Liveness', 'Valence', 'Tempo', 'Duration_ms']
+X = train_df[use_list].to_numpy()
+X_test = test_df[use_list].to_numpy()
 
-prob  = problem(data_list[0], data_list[1])
-param = parameter('-s 11 -c 4 -B 1')
-m = train(prob, param)
+# drop_list = ['Danceability', 'id']
+# X = train_df.drop(drop_list, axis = 1).values
+
+# print(y)
+# print(X)
+# print(X_test)
+
+kf = KFold(n_splits=5, random_state=42, shuffle=True)
+predictions_array = []
+CV_score_array    = []
+for train_index, valid_index in kf.split(X):
+    X_train, X_valid = X[train_index], X[valid_index]
+    y_train, y_valid = y[train_index], y[valid_index]
+    regressor =  HistGradientBoostingRegressor(loss='absolute_error')
+    regressor.fit(X_train, y_train)
+    predictions_array.append(regressor.predict(X_test))
+    CV_score_array.append(mean_absolute_error(y_valid,regressor.predict(X_valid)))
+predictions = np.mean(predictions_array,axis=0)
+predictions[predictions<0] = 0
+predictions[predictions>9] = 9
+print("The average CV mean absolute error is %d" % np.mean(CV_score_array,axis=0))
+sample_df.iloc[:,1:] = np.rint(predictions)
+sample_df.to_csv('submission.csv',index=False)
