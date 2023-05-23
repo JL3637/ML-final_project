20e10fadfce5a5b925904cec392443f6ae7c5953
jason <jason36375667@gmail.com>
2023-05-23 18:13:39 +0800

    good

--- html2023-spring-final-project/main.py
+++ html2023-spring-final-project/main.py
@@ -2,11 +2,17 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import VotingRegressor
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures
 
 train_df = pd.read_csv('train.csv')
+train_df = train_df.fillna(0)
 test_df = pd.read_csv('test.csv')
+test_df = test_df.fillna(0)
 sample_df = pd.read_csv('submission.csv')
 
 y = train_df[['Danceability']].to_numpy().squeeze()
@@ -15,12 +21,8 @@ use_list = ['Energy', 'Key', 'Loudness', 'Speechiness', 'Acousticness', 'Instrum
 X = train_df[use_list].to_numpy()
 X_test = test_df[use_list].to_numpy()
 
-# drop_list = ['Danceability', 'id']
-# X = train_df.drop(drop_list, axis = 1).values
-
-# print(y)
-# print(X)
-# print(X_test)
+poly = PolynomialFeatures(3)
+poly.fit_transform(X)
 
 kf = KFold(n_splits=5, random_state=42, shuffle=True)
 predictions_array = []
@@ -28,7 +30,9 @@ CV_score_array    = []
 for train_index, valid_index in kf.split(X):
     X_train, X_valid = X[train_index], X[valid_index]
     y_train, y_valid = y[train_index], y[valid_index]
-    regressor =  HistGradientBoostingRegressor(loss='absolute_error')
+    # regressor = HistGradientBoostingRegressor()
+    regressor = LinearRegression()
+    # regressor = RandomForestRegressor()
     regressor.fit(X_train, y_train)
     predictions_array.append(regressor.predict(X_test))
     CV_score_array.append(mean_absolute_error(y_valid,regressor.predict(X_valid)))
@@ -36,5 +40,5 @@ predictions = np.mean(predictions_array,axis=0)
 predictions[predictions<0] = 0
 predictions[predictions>9] = 9
 print("The average CV mean absolute error is %d" % np.mean(CV_score_array,axis=0))
-sample_df.iloc[:,1:] = np.rint(predictions)
+sample_df['Danceability'] = np.rint(predictions)
 sample_df.to_csv('submission.csv',index=False)
