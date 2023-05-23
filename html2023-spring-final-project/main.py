45b4c294588afa133a0283c03514d87bc0519bfe
jason <jason36375667@gmail.com>
2023-05-23 20:54:10 +0800

    good

--- html2023-spring-final-project/main.py
+++ html2023-spring-final-project/main.py
@@ -22,8 +22,9 @@ use_list = ['Energy', 'Key', 'Loudness', 'Speechiness', 'Acousticness', 'Instrum
 X = train_df[use_list].to_numpy()
 X_test = test_df[use_list].to_numpy()
 
-poly = PolynomialFeatures(3)
-poly.fit_transform(X)
+# poly = PolynomialFeatures(2)
+# X = poly.fit_transform(X)
+# X_test = poly.fit_transform(X_test)
 
 kf = KFold(n_splits=10, random_state=42, shuffle=True)
 predictions_array = []
@@ -31,15 +32,15 @@ CV_score_array    = []
 for train_index, valid_index in kf.split(X):
     X_train, X_valid = X[train_index], X[valid_index]
     y_train, y_valid = y[train_index], y[valid_index]
-    regressor = HistGradientBoostingRegressor()
+    # regressor = HistGradientBoostingRegressor()
     # regressor = LinearRegression()
-    # regressor = RandomForestRegressor()
+    regressor = RandomForestRegressor()
     regressor.fit(X_train, y_train)
     predictions_array.append(regressor.predict(X_test))
     CV_score_array.append(mean_absolute_error(y_valid,regressor.predict(X_valid)))
 predictions = np.mean(predictions_array,axis=0)
 predictions[predictions<0] = 0
 predictions[predictions>9] = 9
-print(f"CV scaore {CV_score_array}")
+print(f"CV score {np.mean(CV_score_array)}")
 sample_df['Danceability'] = np.rint(predictions)
 sample_df.to_csv('submission.csv',index=False)
